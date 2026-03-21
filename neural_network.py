9957c17f1f7426bfdbce296990619ec5e265e69a
Most <naum20.09@mail.ru>
2026-03-21 23:27:36 +0300

    Add google gemma to project

---
+++ neural_network.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+response = requests.post(
+  url="https://openrouter.ai/api/v1/chat/completions",
+  headers={
+    "Authorization": "Bearer <OPENROUTER_API_KEY>",
+    "Content-Type": "application/json",
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-OpenRouter-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+  },
+
+  data=json.dumps({
+    "model": "google/gemma-3-12b-it",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": n
+          },
+
+        ]
+      }
+    ]
+  })
+)
+
+
+n = input()
+
+if response.status_code == 200:
+  jsn = response.json()
+  res = jsn['choice']['choices'][0]['message']['content']
+  print("Ответ модели:")
+  print(res)
