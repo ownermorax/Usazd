106704dabe2c5e58d0993a57f2e52f3a23be2c16
dark7es <r0manpylaev@yandex.ru>
2026-04-12 01:03:18 +0300

    Add [В админ-панель добавлена функция изменения баланса пользователя, также немного переделана логика хранения баланса(через библиотеку django-money)] main/templates/header.html   main/models.py   main/admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from unfold.admin import ModelAdmin, StackedInline
+from unfold.widgets import UnfoldAdminMoneyWidget
+from .models import Profile
+from djmoney.models.fields import MoneyField
 
-# Register your models here.
+class UnfoldUserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
+
+class ProfileInline(StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Профиль и валюта'
+    fk_name = 'user'
+    formfield_overrides = {
+        MoneyField: {'widget': UnfoldAdminMoneyWidget},
+    }
+    fields = ('name', 'role', 'description', 'avatar', 'balance')
+
+UnfoldUserAdmin.inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UnfoldUserAdmin)
--- main/models.py
+++ main/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
+from djmoney.models.fields import MoneyField
+from djmoney.money import Money
+
+
 class Profile(models.Model):
     """Модель профиля"""
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, default='username')
     role = models.CharField(max_length=20, default='user')
     description = models.TextField(blank=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-    currency = models.IntegerField(default=1000)
+
+    balance = MoneyField(
+        max_digits=10,
+        decimal_places=2,
+        default_currency='USD',
+        default=1000
+    )
+
     def __str__(self):
         return self.user.username
 
     def update_balance(self, money):
-        self.currency += money
+        if isinstance(money, (int, float)):
+            money = Money(money, self.balance.currency)
+        self.balance += money
         self.save()
 
 
